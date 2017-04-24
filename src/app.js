0e4a2a38275da5328b9c3b28b06f3fe334eb70e9
Ellen Probst <ellenprobst@hotmail.com>
2017-04-24 17:00:03 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("hello")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import questions from './components/questions.js';
+
+class App extends React.Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return (
+			<h1>Hello</h1>
+
+		)
+	}
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ src/components/questions.js
@@ -0,0 +1,40 @@
+const questions = [
+	{
+		q: "Did you minify your css file?"
+	},
+	{
+		q: "Did you minify your JS file?"
+	},
+	{
+		q: "Did you scale images?"
+	},
+	{
+		q: "Did you resize images?"
+	},
+	{
+		q: "Can you replace any images with css?"
+	},
+	{
+		q: "Did you use less than 3 fonts?"
+	},
+	{
+		q: "Is everything needed?"
+	},
+	{
+		q: "Are you using CDN?"
+	},
+	{
+		q: "Is there any unused css?"
+	},
+	{
+		q: "Do you switch images based on display density or viewport size?"
+	},
+	{
+		q: "Are there any broken urls?"
+	},
+	{
+		q: "Are there any unnecessary redirects?"
+	}
+]
+
+export default questions;
